49565da71de888d6892c2d5add441e561bdcf41a
git <chenwj@iwgroup.com.cn>
2019-11-01 16:43:33 +0800

    lect_01 turtle 绘制五角星

---
+++ lect_01.py
@@ -0,0 +1,24 @@
+"""
+    作者：陈万军
+    功能：五角星的绘制
+    版本：1.0
+    日期：01/11/2019
+"""
+import turtle
+
+def main():
+    """
+        主函数
+    """
+    # 计数器
+    count = 1
+
+    while count <= 5:
+        turtle.forward(200)
+        turtle.right(144)
+        count = count + 1
+
+    turtle.exitonclick()
+
+if __name__ == '__main__':
+    main()
